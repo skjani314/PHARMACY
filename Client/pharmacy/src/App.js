134ac2e9d13cfdd1a660af1030fed371555b017f
Shaik Mahammad jani <skskjani7@gmail.com>
2024-12-09 20:52:43 +0530

    test 1.0

--- Client/pharmacy/src/App.js
+++ Client/pharmacy/src/App.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home';
 import Context from './context/Context';
-import {Switch,Route,BrowserRouter } from 'react-router-dom';
-import {useState,useEffect} from 'react';
-import {message} from 'antd';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { message } from 'antd';
 import Forgotpass from './components/Login/Forgotpass';
 import axios from 'axios';
 import 'bootstrap';
@@ -15,40 +15,38 @@ import StockPage from './pages/StockPage';
 import StudentPage from './pages/StudentPage';
 import TransactionsPage from './pages/TransactionsPage';
 
-const App=()=> {
-const [activeTab,setActiveTab]=useState('DASHBOARD');
-const [user,setUser]=useState(null);
-const [loading,setLoading]=useState(false);
-const [Medicine_data, setMedData] = useState([]);
-const [search_result,setSerachResult]=useState([]);  
+const App = () => {
+  const [activeTab, setActiveTab] = useState('DASHBOARD');
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [Medicine_data, setMedData] = useState([]);
+  const [search_result, setSerachResult] = useState([]);
 
-const [messageApi, contextHolder] = message.useMessage();
-let flag=false;
-const success = (msg) => {
-  messageApi.open({
-    type: 'success',
-    content: msg,
-  });
-};
-const error = (msg) => {
-  messageApi.open({
-    type: 'error',
-    content: msg,
-  });
-};
+  const [messageApi, contextHolder] = message.useMessage();
+  let flag = false;
+  const success = (msg) => {
+    messageApi.open({
+      type: 'success',
+      content: msg,
+    });
+  };
+  const error = (msg) => {
+    messageApi.open({
+      type: 'error',
+      content: msg,
+    });
+  };
 
-useEffect(()=>{
-  
-  const getUser=async ()=>
-    {
-    try{
+  useEffect(() => {
 
-         const result= await axios.post('/get-user');
-             setUser(result.data);
-             flag=true;
+    const getUser = async () => {
+      try {
+
+        const result = await axios.post('/get-user');
+        setUser(result.data);
+        flag = true;
       }
-      catch(err)
-      {
+      catch (err) {
         return null;
       }
 
@@ -58,35 +56,42 @@ useEffect(()=>{
       setLoading(true);
       try {
 
-          const med_data = await axios.get('/medicine', { name:"" });
-          
-          setMedData([...med_data.data]);
-            console.log(med_data);
+        const med_data = await axios.get('https://pharmacy-production-6114.up.railway.app/medicine', {
+          name: "", 
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        });
+         const x=await med_data.json();
+         console.log(x);
+        setMedData([...med_data.data]);
+        console.log(med_data);
       }
       catch (err) {
-          console.log(err);
-          error("something went wrong")
+        console.log(err);
+        error("something went wrong")
       }
       setLoading(false);
 
+    }
+
+    if (!flag) {
+      getUser();
+      med_fun();
+    }
+    return () => { flag = true; }
+  }, [])
+
+
+
+  const changeActiveTab = (tabId) => {
+    setActiveTab(tabId);
   }
 
-if(!flag){
-getUser();
-med_fun();
-}
-return ()=>{flag=true;}
-},[])
-
-
-
-const changeActiveTab=(tabId)=>{
-      setActiveTab(tabId);
-}
-
-const context_data={
+  const context_data = {
     activeTab,
-    changeActiveTab:changeActiveTab,
+    changeActiveTab: changeActiveTab,
     success,
     error,
     contextHolder,
@@ -99,30 +104,30 @@ const context_data={
     search_result,
     setSerachResult,
 
-}
+  }
 
 
 
-return (
-<Context.Provider value={context_data}>
+  return (
+    <Context.Provider value={context_data}>
 
-  <BrowserRouter>
-  <Switch>
-    <Route exact path='/' component={HomePage}/>
-    <Route exact path='/dashboard' component={user?DashBoardPage:null}/>
-    <Route path='/forgot/:token' component={Forgotpass} />
-    <Route exact path='/medicinepage' component={user?MedicinePage:null}/>
-    <Route exact path='/studentpage' component={user?StudentPage:null}/>
-    <Route exact path='/transactionpage' component={user?TransactionsPage:null}/>
-    <Route exact path='/stockpage' component={user?StockPage:null}/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/dashboard' component={user ? DashBoardPage : null} />
+          <Route path='/forgot/:token' component={Forgotpass} />
+          <Route exact path='/medicinepage' component={user ? MedicinePage : null} />
+          <Route exact path='/studentpage' component={user ? StudentPage : null} />
+          <Route exact path='/transactionpage' component={user ? TransactionsPage : null} />
+          <Route exact path='/stockpage' component={user ? StockPage : null} />
 
-    <Route/>
-  </Switch>
- 
-  </BrowserRouter>
-  
-</Context.Provider>
- );
+          <Route />
+        </Switch>
+
+      </BrowserRouter>
+
+    </Context.Provider>
+  );
 }
 
 export default App;
